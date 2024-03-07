9f3b5ba1d59ca582b3deb0796c63747807561887
HuitaePark <qkrgmlxo3174@gmail.com>
2024-03-08 01:14:55 +0900

    3.8

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,22 +6,26 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 public class MemberService {
-    private final MemberRepository memberRepository = new
-            MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public MemberService(MemoryMemberRepository memberRepository) {
+    }
+
     /**
      * 회원가입
      */
     public Long join(Member member) {
         validateDuplicateMember(member);//중복 회원 검증 memberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
+
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
-
                 .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
+        throw new IllegalStateException("이미 존재하는 회원입니다.");
+    });
+}
     /**
      *전체 회원 조회
      */
@@ -30,4 +34,5 @@ public class MemberService {
     }
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
-    } }
+    }
+}
